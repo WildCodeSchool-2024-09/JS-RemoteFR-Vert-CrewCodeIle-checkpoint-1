e7a845c31fc25d1efcb2e5d26a76e306decebe9f
Flam64 <celio.mozes.rocha@gmail.com>
2024-10-21 10:04:36 +0200

    Feat : finish algo1 in part 1

--- algo/1/thirdAngle.js
+++ algo/1/thirdAngle.js
@@ -5,9 +5,12 @@ Ex:
 thirdAngle(90, 30) doit retourner 60
 thirdAngle(20, 80) doit retourner 80
 */
+import jest from "jest";
 
 function thirdAngle(a, b) {
   // Your code here !
+  const result = 180 - (a + b);
+  return result;
 }
 
 module.exports = thirdAngle;
