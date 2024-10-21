d03bfbe76938a120c4d87c432416674a7cd2a5b1
Sanesthesie <victorsanlopez@gmail.com>
2024-10-21 13:27:24 +0200

    finish js step 2  in part 3

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -2,3 +2,17 @@ const avatarImage = document.getElementById("avatar");
 avatarImage.addEventListener("click", function () {
 	avatarImage.src = "image/avatar.svg";
 });
+
+const replaceNameButton = document.createElement("Button");
+const sectionButton = document.querySelector(".description");
+const textButton = document.createTextNode("modify text and color");
+sectionButton.appendChild(replaceNameButton);
+replaceNameButton.appendChild(textButton);
+
+replaceNameButton.addEventListener("click", function () {
+	// prompt("Enter a color");
+	const firstName = prompt("Enter your name");
+	const firstNameSpan = document.getElementById("firstname");
+	firstNameSpan.textContent = firstName;
+	firstNameSpan.style.color = "white";
+});
