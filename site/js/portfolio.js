1ef5be69a333530b6bf484f085b60897c57f58e4
LEFEVER Jean-Philippe <lefever.jp@hotmail.fr>
2024-10-21 13:55:06 +0200

    feat : function JS / Text HTML

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -4,20 +4,21 @@ avatar.onclick = function() {
     avatar.src ="image/avatar.svg"
 }
 
-const firstName = document.querySelector("#firstname")
-const buttonText = document.querySelector(".modifyText")
-const userColor = document.querySelector(".description")
-const ChangeAllColor = document.querySelector(".pink-text")
+const buttonText = document.querySelector(".modifyText");
+
 
 
 buttonText.addEventListener('click',function(){
+    const firstName = document.querySelector("#firstname");
     const userName= prompt('Entrez votre nom')
     firstName.textContent = `${userName}`
+    firstName.style.color='white'
     
-   
+    const userColor = document.querySelector(".description");
     const backGround = prompt('Choisissez une couleur')
     userColor.style.backgroundColor = `${backGround}`
-
+    
+    const ChangeAllColor = document.querySelectorAll(".pink-text");
     const colorOfElement = prompt('Choisissez une couleur')
     ChangeAllColor.style.color = `${colorOfElement}`
 }
